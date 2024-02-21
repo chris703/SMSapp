edabd3c7dfc75f5e97d44a5786246dff7e11b9c3
chris703 <filipo1409@gmail.com>
2024-02-21 12:49:24 -0500

    Update credentials

--- src/twilio/send-sms.js
+++ src/twilio/send-sms.js
@@ -1,7 +1,9 @@
 const config = require ('../config');
-const client = require('twilio')('AC875307f64b4ca7ff113574240fd32e1a', 'e10b0722faf5682ef79293eb51185332');
+//const client = require('twilio')('AC875307f64b4ca7ff113574240fd32e1a', 'e10b0722faf5682ef79293eb51185332');
 //const client = require('twilio')('AC875307f64b4ca7ff113574240fd32e1a', '913a083d6a9307888ce913ffa0d8648f');
 //const client = require('twilio')(config.accountSid, config.authToken);
+const client = require('twilio')('AC875307f64b4ca7ff113574240fd32e1a', '3f2dcebafd7a123299f97e3ef95e3fef');
+
 
 /**
  * Send a SMS message
@@ -22,7 +24,7 @@ async function sendMessage (body, phone){
         
     } catch (error) {
         console.log( "aqui es el error" + error );
-        console.log("credenciales" + client.username);
+        console.log("credenciales" + " " + client.username + client.password);
     }
    console.log(message.from);
 
